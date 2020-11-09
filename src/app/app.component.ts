de70e095201cbb1c3d772a87cc3c4d19c42cb453
Ibrahim Abdelhamed <i_abdulhamid@outlook.com>
2020-11-09 10:42:49 +0200

    adding application pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,27 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
+      title: 'About Us',
+      url: '/about',
+      icon: 'information-circle'
+    },
+    {
+      title: 'Menu',
+      url: '/menu',
+      icon: 'list'
+    },
+    {
+      title: 'Contact Us',
+      url: '/contact',
       icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Family', 'Friends', 'Notes', 'Work'];
 
   constructor(
     private platform: Platform,
